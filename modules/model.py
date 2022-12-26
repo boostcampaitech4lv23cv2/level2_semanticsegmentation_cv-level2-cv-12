390064aca69bd1029a6d07ac17a2f773362a9397
sangho <chointer04@gmail.com>
2022-12-26 16:04:54 +0000

    add model: eff_b4 + deeplab_v3+

--- modules/model.py
+++ modules/model.py
@@ -4,6 +4,7 @@ from .models.Deconvnet_jj import DeconvNet_JJ
 from .models.FCN_16s import FCN16s
 from .models.Efficientb0_unet_pp import Efficientb0_UNet_PP
 from .models.Efficientb4_unet_pp import Efficientb4_UNet_PP
+from .models.Efficientb4_deeplabv3p import Efficientb4_Deeplab_v3p
 
 _model_entrypoints = {
     "efficient_unet": Efficient_UNet,
@@ -12,6 +13,7 @@ _model_entrypoints = {
     "fcn_16s": FCN16s,
     "efficientb0_unet_pp": Efficientb0_UNet_PP,
     "efficientb4_unet_pp": Efficientb4_UNet_PP,
+    "efficientb4_deeplabv3p": Efficientb4_Deeplab_v3p,
 }
 
 def model_entrypoint(model_name):
